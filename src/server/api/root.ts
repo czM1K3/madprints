d385689052a125047ada26d25e007747140f4639
czM1K3 <misamadera1@gmail.com>
2024-07-26 10:59:55 +0200

    Removed t3 default router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { publicRouter } from "./routers/public";
 import { modelRouter } from "./routers/model";
@@ -9,7 +8,6 @@ import { modelRouter } from "./routers/model";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   public: publicRouter,
   model: modelRouter,
 });
