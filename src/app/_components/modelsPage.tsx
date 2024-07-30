beac52ce940d79e6383e8072453d0ae6e339b326
czM1K3 <misamadera1@gmail.com>
2024-07-30 22:24:51 +0200

    Merged pages to root

--- src/app/_components/modelsPage.tsx
+++ src/app/_components/modelsPage.tsx
@@ -1,32 +1,50 @@
-import React, { type FC } from "react";
-import { api } from "~/trpc/server";
-import { ModelCard } from "./card";
-import { Center, SimpleGrid } from "@mantine/core";
-import { ModelsPagination } from "./pagination";
-import { notFound } from "next/navigation";
+"use client";
 
+import React, { useState, type FC } from "react";
+import { ModelCard } from "./card";
+import { Center, Pagination, SimpleGrid, Text } from "@mantine/core";
+import { api } from "~/trpc/react";
 
 type ModelsPageProps = {
-  page: number;
-}
+  initialData: {
+    models: {
+        id: string;
+        description: string;
+        title: string;
+    }[];
+    pages: number;
+  };
+};
 
-export const ModelsPage: FC<ModelsPageProps> = async ({ page }) => {
-  const models = await api.public.modelsPage({ page });
-  if (models.models.length < 1)
-    return notFound();
+export const ModelsPage: FC<ModelsPageProps> = ({ initialData }) => {
+  const [page, setPage] = useState(1);
+
+  const { data, isLoading, error } = api.public.modelsPage.useQuery({ page }, {
+    initialData,
+    initialDataUpdatedAt: 0,
+  });
+  if (isLoading)
+    return (
+      <Text>Loading</Text>
+    );
+  if (Boolean(error) || !data || data.models.length < 1)
+    // return notFound();
+    return (
+      <Text>Error</Text>
+    );
   return (
     <>
       <SimpleGrid
         cols={{ base: 1, sm: 2, md: 3 }}
       >
-        {models.models.map((model) => (
+        {data.models.map((model) => (
           <Center key={model.id}>
             <ModelCard description={model.description} id={model.id} title={model.title} />
           </Center>
           ))}
       </SimpleGrid>
       <Center p="sm">
-        <ModelsPagination currentPage={page} maxPages={models.pages} />
+        <Pagination total={data.pages} value={page} onChange={(v) => setPage(v)} />
       </Center>
     </>
   );
