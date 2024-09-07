55c7fc394d385eddf8a98052353e72c458bea552
czM1K3 <misamadera1@gmail.com>
2024-09-07 08:50:37 +0200

    Rounding of number on slow devices

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -1,1 +1,1 @@
-export const msToString = (time: number) => Math.floor(time / 1000) + " seconds " + (time % 1000) + " milliseconds";
+export const msToString = (time: number) => Math.floor(time / 1000) + " seconds " + Math.floor(time % 1000) + " milliseconds";
